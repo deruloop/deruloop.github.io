b9d3ebf8a24c1b952842217dff112a9cdf93b4bd
Cristiano Personale <c.calicchia@icloud.com>
2021-11-17 00:04:16 +0100

    shiba fetching working as intented

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-    $.getJSON("https://www.chain.so/api/v2/address/DOGE/DK1i69bM3M3GutjmHBTf7HLjT5o3naWBEk", function(data) { $('#totaldonations').text(data.data.balance); });
-    // $.getJSON("https://www.chain.so/api/v2/address/DOGE/DK1i69bM3M3GutjmHBTf7HLjT5o3naWBEk", function(data) { $('#totaldonations').text(data.data.balance); });
-    // $('#TotalRaised').load('http://dogechain.info/chain/Dogecoin/q/getreceivedbyaddress/DK1i69bM3M3GutjmHBTf7HLjT5o3naWBEk'); });
+    $.getJSON("https://www.chain.so/api/v2/address/DOGE/DDFQ15ErrUZxUfSCPfotL7jVar64UuvTRq", function(data) { $('#totaldonationsdoge').text(data.data.balance); });
+
+    web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/5d62c251719e40f1ae2e87a0e2e5eb65'));
+    console.log('Getting contract tokens balance.....');
+    var addr = ('0x26928E56d5c46C89434d570e40BA8230FfD5C8E6');
+    console.log("Address: " + addr);
+    var contractAddr = ('0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce'); //SHIBA
+    var tknAddress = (addr).substring(2);
+    var contractData = ('0x70a08231000000000000000000000000' + tknAddress);
+    web3.eth.call({
+    to: contractAddr,
+    data: contractData
+    }, function(err, result) {
+    	if (result) {
+    		var tokens = web3.utils.toBN(result).toString();
+    		console.log('Tokens Owned: ' + web3.utils.fromWei(tokens, 'ether'));
+        $('#totaldonationsshiba').text(web3.utils.fromWei(tokens, 'ether'));
+    	}
+    	else {
+    		console.log(err); // Dump errors here
+    	}
+    });
+
 });
